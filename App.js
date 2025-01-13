4d95fde40f35b32213f87246cd0614509a19c04e
sharada0417 <sharada.kanchana2001@gmail.com>
2025-01-14 03:18:27 +0530

    fix errors

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Home from './components/Home';
+import Profile from './components/Profile';
 import ProfileTap from './components/ProfileTap';
 
-
-const App = () => {
+export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='home' screenOptions={{
-          headerStyle:{backgroundColor:'#4b0150'},
-          headerTintColor:'#ffff',
-          headerTitleAlign:'center'
+        <Stack.Navigator initialRouteName='home' screenOptions={
+          {
+            headerStyle: { backgroundColor: '#4b0150' },
+            headerTintColor:'#ffff', 
+            headerTitleAlign:'center'
         }}>
-          <Stack.Screen name='home' component={Home} options={{ title: "Uov Heath Care"}}/>
-          <Stack.Screen name="profiletap" component={ProfileTap} options={{ title: "Student Health Profile"}}/>
+          <Stack.Screen name='home' component={Home} options={{ title: "UoV Health Care" }} />
+          <Stack.Screen name='profiletap' component={ProfileTap} options={{ title: "Student Health Profile" }} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
+    
   );
-};
-
-export default App;
+}
--- components/ProfileTap.js
+++ components/ProfileTap.js
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const ProfileTap = () => {
-  return (
-    <View>
-      <Text>ProfileTap</Text>
-    </View>
-  )
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Profile from './Profile';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Health from './Health';
+import BMI from './BMI';
+export default function ProfileTap({ route }){
+    const Tab = createBottomTabNavigator();
+    const { student } = route.params;
+    console.log(student);
+    return (
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused }) => {
+              let iconName;
+  
+              if (route.name === 'Profile') {
+                iconName = focused ? 'account' : 'account-outline';
+              } else if (route.name === 'Health') {
+                iconName = focused ? 'hospital-box' : 'hospital-box-outline';
+              } else if (route.name === 'BMI') {
+                iconName = focused ? 'file-chart' : 'file-chart-outline';
+              }
+  
+              return <Icon name={iconName} size={24} color="#4b0150" />;
+            },
+            tabBarLabelStyle:{color:"#4b0150"},
+            headerShown:false,
+          })}>
+          <Tab.Screen name="Profile" component={Profile} initialParams={{student:student}} options={{tabBarLabel: 'Profile' }}/>
+          <Tab.Screen name="Health" initialParams={{sid:student.id,sname:student.name}} component={Health} />
+          <Tab.Screen name="BMI" initialParams={{sid:student.id,sname:student.name}} component={BMI} />
+        </Tab.Navigator>
+      );
 }
-
-export default ProfileTap
