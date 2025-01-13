47acc2757578506cb0a6f9e6d634e49affa0dcbe
sharada0417 <sharada.kanchana2001@gmail.com>
2025-01-13 13:22:38 +0530

    update Homepage

---
+++ components/ProfileTap.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProfileTap = () => {
+  return (
+    <View>
+      <Text>ProfileTap</Text>
+    </View>
+  )
+}
+
+export default ProfileTap
